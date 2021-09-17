006ec96430f4da3046296b0255e228ede88bd245
Jongbin Kim <jongbinkim@hanyang.ac.kr>
2021-09-17 17:33:28 +0900

    Modified license

--- storage/innobase/pleaf/pleaf_bufpage.cc
+++ storage/innobase/pleaf/pleaf_bufpage.cc
@@ -1,36 +1,26 @@
-/*****************************************************************************
-
-Copyright (c) 1994, 2020, Oracle and/or its affiliates. All Rights Reserved.
-Copyright (c) 2021, Anonymous Lab.
-
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License, version 2.0, as published by the
-Free Software Foundation.
-
-This program is also distributed with certain software (including but not
-limited to OpenSSL) that is licensed under separate terms, as designated in a
-particular file or component or in included license documentation. The authors
-of MySQL hereby grant you an additional permission to link the program and
-your derivative works with the separately licensed software that they have
-included with MySQL.
-
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License, version 2.0,
-for more details.
-
-You should have received a copy of the GNU General Public License along with
-this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
-
-*****************************************************************************/
-
-/** @file pleaf/pleaf_bufpage.cc 
- P-Leaf Buffer Page Structure
-
- Created 4/18/2021 Anonymous
- *******************************************************/
-
+/*-------------------------------------------------------------------------
+ *
+ * pleaf_bufpage.cc
+ * 		Internal operation in pleaf buffer pages 
+ *
+ * 
+ * Copyright (C) 2021 Scalable Computing Systems Lab.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *-------------------------------------------------------------------------
+ */
 #ifdef J3VM
 
 #include <assert.h>
